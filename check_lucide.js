96de2d286a92818e8ebc547091604b7ae6ed0032
Bony Koshy <bonykoshy@gmail.com>
2026-08-03 01:20:13 +0530

    refactor: remove Vite configuration, clean up gitignore, and enable legacy-peer-deps for build environment

--- check_lucide.js
+++
@@ -1,7 +0,0 @@
-import * as lucide from "lucide-react";
-const keys = Object.keys(lucide);
-console.log("Total keys:", keys.length);
-console.log(
-  "Keys containing git:",
-  keys.filter((k) => k.toLowerCase().includes("git"))
-);
